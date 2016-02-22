dfba55e09241bcdfcf464dd617393d7da0acc67e
nmkim <nmkim@umich.edu>
2016-02-22 13:18:45 -0500

    added hoop logic

---
+++ Assets/Hoop.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hoop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D col) {
+		if (col.gameObject.tag == "Ball") {
+			float ball_ypos = col.gameObject.transform.position.y;
+			if (ball_ypos > gameObject.transform.position.y) {
+				print ("scored!");
+			}
+		}
+	}
+}
