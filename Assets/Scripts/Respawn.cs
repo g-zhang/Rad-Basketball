84853986e5ef74f0d9decaa5c5a77370b339bffe
nmkim <nmkim@umich.edu>
2016-02-22 18:45:51 -0500

    added respawn script

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Respawn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.transform.position.y < -12.0f) {
+			gameObject.transform.position = Vector2.zero;
+		}
+	}
+}
