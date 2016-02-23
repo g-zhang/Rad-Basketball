8775720d80183f83a32b30e9314f11e6fb735a1e
Gordon <gordonz@umich.edu>
2016-02-22 20:41:32 -0500

    ball script physics stuff fixing

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+
+    public GameObject owner;
+
+	// Use this for initialization
+	void Start () {
+        owner = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
