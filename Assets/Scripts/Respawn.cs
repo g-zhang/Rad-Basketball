d8c8d86c0a1cebff85ff27dbdca8e1246fe74f24
nmkim <nmkim@umich.edu>
2016-02-25 13:50:32 -0500

    merged with xplatform and updated respawn script

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -11,7 +11,17 @@ public class Respawn : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (gameObject.transform.position.y < -12.0f) {
-			gameObject.transform.position = Vector2.zero;
+			if (gameObject.tag == "Ball") {
+				if (gameObject.GetComponent<BallController>().owner == null) {
+					gameObject.transform.position = Vector2.zero;
+				}
+			}
+
+			else {
+				Vector2 new_pos = new Vector2 (gameObject.transform.position.x, 6.0f);
+				gameObject.transform.position = new_pos;
+				gameObject.GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
+			}
 		}
 	}
 }
