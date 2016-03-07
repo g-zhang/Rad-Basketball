4c78090610a3ce7be447e7b1c41387fc64abd6ec
nmkim <nmkim@umich.edu>
2016-03-07 15:45:18 -0500

    added powerup controller/spawner

---
+++ Assets/Scripts/PowerUp_Controller.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUp_Controller : MonoBehaviour {
+
+	public GameObject p_double_jump;
+	public GameObject p_extra_ball;
+
+	float counter = 0.0f;
+	float checker = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() {
+
+		checker = Random.Range (1500.0f, 2000.0f);
+
+		if (counter < checker) {
+			counter++;
+		} else { 
+
+			counter = 0.0f;
+
+			float power_picker = Random.Range (-2.0f, 2.0f);
+			if (power_picker < 0) {
+				GameObject temp_p = Instantiate(p_double_jump);
+				temp_p.transform.position = new Vector2 (Random.Range (-8.0f, 6.0f), 7.5f);
+				temp_p.transform.SetParent (this.transform);
+
+			} else {
+				GameObject temp_p = Instantiate(p_extra_ball);
+				temp_p.transform.position = new Vector2 (Random.Range (-8.0f, 6.0f), 7.5f);
+				temp_p.transform.SetParent (this.transform);
+			}
+		}
+			
+	}
+}
