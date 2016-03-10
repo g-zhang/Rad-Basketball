07d05c2ebfd82ca7ca7aae5f3a406249e2704480
John DeSilva <desilvjo@umich.edu>
2016-03-10 13:52:18 -0500

    Loop lone

---
+++ Assets/Music.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Music : MonoBehaviour {
+
+    public AudioSource audio;
+
+    static bool AudioBegin = false;
+
+    void Awake()
+    {
+        if (!AudioBegin) {
+            audio.Play();
+            DontDestroyOnLoad(gameObject);
+            AudioBegin = true;
+        }
+    }
+}
